481376f9795a13b3a33440acecdc79c027492897
Indra Doerga <0957822@hr.nl>
2022-04-19 01:44:48 +0200

    Set Timer gandolf move

--- dev/game.ts
+++ dev/game.ts
@@ -107,7 +107,9 @@ class Game {
         // AI needs to make a move if it is not the player's turn
         if (!this.playerTurn) {
             
+            setTimeout(() => { // wait for Gandalf before knights move.
             GameAI.moveKnight(this.king, this.knights, this.gameState);
+            }, 2000)
             this.playerTurn = true;
 
             // check lose
